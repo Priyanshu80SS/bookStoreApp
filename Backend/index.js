cc4d379192cda63689b3d67b2e1779bb66ecb02d
Priyanshu Bhardwaj <125396693+Priyanshu80SS@users.noreply.github.com>
2024-05-28 19:39:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://book-store-client-ruby.vercel.app"],
+    origin: ["https://bookstoreappclient.vercel.app"],
     mthods:["POST","GET"],
     credentials:true
   }
