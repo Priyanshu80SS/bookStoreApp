296fd8027f9af810bba995d8534dff913167e5de
Priyanshu Bhardwaj <125396693+Priyanshu80SS@users.noreply.github.com>
2024-05-28 19:36:15 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ const Course = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://deploy-mern-api-iota.vercel.app/book");
+        const res = await axios.get("https://bookstoreapi-omega.vercel.app/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
