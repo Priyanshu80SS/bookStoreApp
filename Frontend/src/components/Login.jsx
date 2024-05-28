74ac0ec1cfb4d4e0e58b9bbcd8e8d9ea8e8a60e5
Priyanshu Bhardwaj <125396693+Priyanshu80SS@users.noreply.github.com>
2024-05-28 19:35:27 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
         password: data.password,
       };
       await axios
-        .post("https://deploy-mern-api-iota.vercel.app/users/login", user)
+        .post("https://bookstoreapi-omega.vercel.app/users/login", user)
         .then((res) => {
           console.log(res.data);
           if (res.data) {
